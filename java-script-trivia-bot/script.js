904cf61fce7020cc9e7100433419487f484c4ea1
Deborahfregene-tech <debbiefregene97@gmail.com>
2026-07-24 10:08:24 +0100

    Create script.js

---
+++ java-script-trivia-bot/script.js
@@ -0,0 +1,15 @@
+console.log("Hello! I'm your coding fun fact guide!");
+const botName = "Deborah";
+const botLocation = "Mars";
+const favoriteLanguage = "HTML";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact =  favoriteLanguage + " is Not a Programming Language: It's a markup language designed to structure and present content on the web.";
+console.log(codingFact);
+codingFact = favoriteLanguage + " was created in the early 90s by Tim Berners-Lee.";
+console.log(codingFact);
+codingFact = "Every website uses " + favoriteLanguage + " to work." + ".";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+
+  
